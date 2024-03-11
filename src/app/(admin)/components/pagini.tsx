6b1937383653bc89c3f71ad517a8450e217e0d5b
Ionut767 <ionutiacob650@gmail.com>
2024-03-11 23:21:28 +0200

    Feat: Deleted useless pages

--- src/app/(admin)/components/pagini.tsx
+++ src/app/(admin)/components/pagini.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function Pagini() {
-  const pagini = ["Personalitati", "Orase", "Arta", "Statistici"];
+  const pagini = ["Personalitati", "Orase", "Arta"];
   return (
     <div className="w-full md:w-1/4 bg-zinc-950 border-zinc-800 border-[1px] rounded-lg flex flex-col space-y-4 p-5 h-full">
       <div className="border-b border-zinc-800 pb-1">
--- src/app/(admin)/cpannel/statistici/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Statistici() {
-  return <p>Statistici</p>;
-}
